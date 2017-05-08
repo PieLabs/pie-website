2a16992a060007f8b53951fa3e27dbaa0061a6e8
evaneus <evan.eustace@gmail.com>
2017-05-08 10:50:24 -0400

    inline svgs, don't use yams for feature row, content tweaks

--- content/index.md
+++ content/index.md
@@ -3,7 +3,7 @@ title: pie framework
 layout: Homepage
 main: 
   title: Pie
-  subtitle: An Open Source framework for building and sharing interactive assessment question types.
+  subtitle: Open Source framework for building and sharing interactive assessment question types and assessment items.
 publishers: 
   text: For content publishers
   subtext: Create content using simple or advanced assessment interactions and distribute it to your customers with the confidence that your content will appear and function exactly as designed by your content authors. 
@@ -12,23 +12,4 @@ developers:
   text: For Developers
   subtext: The PIE Framework is an Open Source non-proprietary  framework that uses web development best practices to allow you to easily create re-usable question types with advanced UI and scoring behavior, and allows you to distribute these question types so they can be used on any system.
   url: assets/dev.png
-features:
-  - image: assets/choice.svg
-    title: Freedom & Choice
-    text: PIE is an Open Source Framework. Any content (or new question types) you create belong to you. You do not require a licence, payment or partnership with any organization to display or share your content. 
-  - image: assets/network.svg
-    title: Simple Distribution
-    text: PIE provides a non-proprietary format for Assement Content that preserves the design and functionality that the content developer intended. Both for simpler question types and more advanced interactions.
-  - image: assets/developer.svg
-    title: Developer Friendly
-    text: Use the capabilities of HTML5 and modern javascript development to deliver engaging, interactive question types that will work on web or mobile devices.
-  - image: assets/author.svg
-    title: Content Authoring
-    text: Integrate Technology Enhanced item authoring to your site, using free Open Source question types. 
-  - image: assets/standards.svg
-    title: Standards Compatible
-    text: The PIE Framework is not itself a standard, but a working set of software tools. It can work within and support a number of existing education technology standards.
-  - image: assets/innovate.svg
-    title: Quality and Innovation
-    text: You can create new and innovative interactions to deliver a more engaging and effective assessment experience. Or create or modify existing question types so they perfectly represent your educational and design standards.   
 ---
--- src/components/Homepage/FeatureRow/index.js
+++ src/components/Homepage/FeatureRow/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './index.css';
-
+import Svg from "react-svg-inline"
+import choiceSvg from "../../../../content/assets/choice.svg"
+import networkSvg from "../../../../content/assets/network.svg"
+import developerSvg from "../../../../content/assets/developer.svg"
+import authorSvg from "../../../../content/assets/author.svg"
+import standardsSvg from "../../../../content/assets/standards.svg"
+import innovateSvg from "../../../../content/assets/innovate.svg"
 
 export default class FeatureRow extends React.Component {
 
@@ -14,19 +20,61 @@ export default class FeatureRow extends React.Component {
         debugger;
 
         return <div className={featureRow}>
-            {features.map(content =>
 
-                    <div className={feature} key={content.title}>
-                        <div className={image}>
-                            <img src={content.image}/>
-                        </div>
-                        <div className={description}>
-                            <h2>{content.title}</h2>
-                            <p>{content.text}</p>
-                        </div>
+                <div className={feature}>
+                    <div className={image}>
+                        <Svg svg={choiceSvg}/>
                     </div>
-
-            )}
+                    <div className={description}>
+                        <h2>Freedom & Choice</h2>
+                        <p>PIE is an Open Source Framework. Any content (or new question types) you create belong to you. You do not require a licence, payment or partnership with any organization to display or share your content.</p>
+                    </div>
+                </div>
+                <div className={feature}>
+                    <div className={image}>
+                        <Svg svg={networkSvg}/>
+                    </div>
+                    <div className={description}>
+                        <h2>Simple Distribution</h2>
+                        <p>PIE provides a non-proprietary format for Assement Content that preserves the design and functionality that the content developer intended. Both for simpler question types and more advanced interactions.</p>
+                    </div>
+                </div>                
+                <div className={feature}>
+                    <div className={image}>
+                        <Svg svg={developerSvg}/>
+                    </div>
+                    <div className={description}>
+                        <h2>Developer Friendly</h2>
+                        <p>Use the capabilities of HTML5 and modern javascript development to deliver engaging, interactive question types that will work on web or mobile devices.</p>
+                    </div>
+                </div>                
+                <div className={feature}>
+                    <div className={image}>
+                        <Svg svg={authorSvg}/>
+                    </div>
+                    <div className={description}>
+                        <h2>Content Authoring</h2>
+                        <p>Integrate Technology Enhanced item authoring to your site, using free Open Source question types. </p>
+                    </div>
+                </div>
+                <div className={feature}>
+                    <div className={image}>
+                        <Svg svg={standardsSvg}/>
+                    </div>
+                    <div className={description}>
+                        <h2>Standards Compatible</h2>
+                        <p>The PIE Framework is not itself a standard, but a working set of software tools. It can work within and support a number of existing education technology standards.</p>
+                    </div>
+                </div>                
+                <div className={feature}>
+                    <div className={image}>
+                        <Svg svg={innovateSvg}/>
+                    </div>
+                    <div className={description}>
+                        <h2>Quality and Innovation</h2>
+                        <p>You can create new and innovative interactions to deliver a more engaging and effective assessment experience. Or create or modify existing question types so they perfectly represent your educational and design standards.</p>
+                    </div>
+                </div> 
 
         </div>
 
