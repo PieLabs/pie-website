5c8416cfa1f970ca4e936148ff718d845325c988
Ed Eustace <ed.eustace@gmail.com>
2017-05-16 16:03:44 +0100

    ui polish

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,13 +1,13 @@
 import React, { PropTypes } from 'react';
+
 import { Link } from 'phenomic';
 import styles from './index.css';
 
 export default (props) => {
   const { to, label } = props;
-  const { root, link } = styles;
-  return <div className={root}>
+  return <div className={styles.root}>
     <Link
-      className={link}
+      className={styles.link}
       to={to}>
       {props.children}
     </Link>
