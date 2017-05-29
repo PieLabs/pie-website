0f1a48c08f6c8ccc3ec65f10b8a1e50efbe4ba2c
Ed Eustace <ed.eustace@gmail.com>
2017-05-29 10:45:49 +0100

    make loader async

--- src/build/js-doc-to-markdown-loader.js
+++ src/build/js-doc-to-markdown-loader.js
@@ -1,17 +1,53 @@
 import jsdoc2md from 'jsdoc-to-markdown';
-import resolve from 'resolve';
+import pathResolve from 'resolve';
 
 export default function (input) {
-  const lines = input.split('\n');
 
-  const out = lines.map(l => {
-    const match = l.match(/\[js_to_md\]\((.*?)\).*/);
-    if (match && match.length > 0) {
-      const path = resolve.sync(match[1]);
-      return jsdoc2md.renderSync({ files: path });
+  const done = this.async();
+
+
+  if (!done) {
+    throw new Error('can only run in async mode.');
+  } else {
+
+    const regex = /\[js_to_md\]\((.*?)\).*/;
+
+    if (!regex.test(input)) {
+      done(null, input);
     } else {
-      return l;
+
+      const process = (s) => {
+        const match = regex.exec(s);
+        if (match === null) {
+          return Promise.resolve(s);
+        } else {
+          const matchSrc = match[0];
+          const group = match[1];
+          return new Promise((resolve, reject) => {
+            pathResolve(group, (err, path) => {
+              if (err) {
+                reject(err);
+              } else {
+                return jsdoc2md.render({ files: path })
+                  .then(result => {
+                    const out = s.replace(matchSrc, result);
+                    return process(out)
+                      .then(o => resolve(o))
+                      .catch(e => reject(e));
+                  })
+                  .catch(e => reject(e));
+              }
+            })
+          });
+        }
+      }
+
+      return process(input)
+        .then(transformed => {
+          done(null, transformed)
+        })
+        .catch(done);
     }
-  });
-  return out.join('\n');
+
+  }
 }
